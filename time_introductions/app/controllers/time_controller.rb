16f0edac21a820574ce665505e0faec9c3d59178
Lucas Henderson <lucasjhenderson@gmail.com>
2015-11-03 17:05:32 -0700

    Fixed method on controller to match route
    [##106520248]

--- time_introductions/app/controllers/time_controller.rb
+++ time_introductions/app/controllers/time_controller.rb
@@ -2,7 +2,7 @@
 class TimeController < ApplicationController
 #{Time.now.utc.strftime("%a, %d %b %Y %T UTC %:z")}
 
-  def time
+  def now
     @now = Time.now.utc.strftime("%a, %d %b %Y %T UTC %:z")
   end
 end
